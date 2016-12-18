d1f6facc020590051eb0066cbd72c24c74045b4d
dc7878 <1026961500@qq.com>
2016-12-18 15:59:26 +0800

    新建Utils和activity包

---
+++ customview/app/src/main/java/com/cc/dc/customview/activity/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.cc.dc.customview.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.cc.dc.customview.R;
+
+/**
+ * Created by dc on 16/12/18.
+ */
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ customview/app/src/main/java/com/cc/dc/customview/utils/DpUtil.java
@@ -0,0 +1,10 @@
+package com.cc.dc.customview.utils;
+
+/**
+ * Created by dc on 16/12/18.
+ */
+public class DpUtil {
+    public static int dpToPx(int dp) {
+        return -1;
+    }
+}
