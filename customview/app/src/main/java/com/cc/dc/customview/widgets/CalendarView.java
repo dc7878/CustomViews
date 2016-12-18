2b74a3189f40648d0083cf592b7108bf53465ef0
dc7878 <1026961500@qq.com>
2016-12-18 15:51:55 +0800

    ‘修改忽略文件’

---
+++ customview/app/src/main/java/com/cc/dc/customview/widgets/CalendarView.java
@@ -0,0 +1,23 @@
+package com.cc.dc.customview.widgets;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by dc on 16/12/18.
+ */
+public class CalendarView extends View {
+
+    public CalendarView(Context context) {
+        super(context);
+    }
+
+    public CalendarView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CalendarView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
