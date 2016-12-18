fc8c013b36b037f49d60e6c8a6d64ede40b3d58a
dc7878 <1026961500@qq.com>
2016-12-18 15:55:57 +0800

    ‘测试提交’

--- customview/app/src/main/java/com/cc/dc/customview/widgets/CalendarView.java
+++ customview/app/src/main/java/com/cc/dc/customview/widgets/CalendarView.java
@@ -1,6 +1,8 @@
 package com.cc.dc.customview.widgets;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -19,5 +21,16 @@ public class CalendarView extends View {
 
     public CalendarView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawColor(Color.YELLOW);
     }
 }
